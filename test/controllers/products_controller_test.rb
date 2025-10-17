c6bcde153558a00be3e2935faaf5bbd227affe25
Otis Lau <otishlau@gmail.com>
2025-10-17 01:15:28 -0400

    fixing github testing stuff

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,12 +2,24 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get products_index_url
+    get products_url
     assert_response :success
   end
 
   test "should get create" do
-    get products_create_url
+    assert_difference("Product.count", 1) do
+      post products_url, params: {
+        product: {
+          name: "Widget",
+          sku: "SKU-NEW",
+          reorder_point: 2,
+          on_hand: 4
+        }
+      }
+    end
+
+    assert_redirected_to products_path
+    follow_redirect!
     assert_response :success
   end
 end
