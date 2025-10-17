d641fcf63d239916c530ca94375cb88096ddefa3
Otis Lau <otishlau@gmail.com>
2025-10-17 00:49:47 -0400

    added most crud funtionalty to the frontend of the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  # products routes (only index and create for now)
-  resources :products, only: [:index, :create]
+  resources :products, only: [:index, :create, :update, :destroy]
 end
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    react(),
+    react({
+      jsxRuntime: 'automatic',
+      babel: {
+        plugins: [],
+      },
+    }),
   ],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 })
