ed615f6496fab8cf1b1c0aa004cd3b29fb328c10
Otis Lau <otishlau@gmail.com>
2025-10-26 20:49:10 -0400

    still fixing testing errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,6 +11,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.email = @user.email.to_s.downcase
+    assign_store(@user)
 
     if @user.save
       session[:user_id] = @user.id
@@ -24,6 +25,16 @@ class RegistrationsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :store_id)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
+
+  def assign_store(user)
+    store_id = params.dig(:user, :store_id)
+    return if store_id.blank?
+
+    store = Store.find_by(id: store_id)
+    return user.store = store if store
+
+    user.errors.add(:store, "must exist")
   end
 end
